7fb2998a69721c43fbdf80b816284806357486b8
Vineel Shah <vineel@vineel.com>
2023-02-21 15:27:40 -0500

    little bit of cleanup

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,11 @@
-import {isVideoTransformDevice} from 'amazon-chime-sdk-js';
-import {join} from './chimeFunctions';
 import { attachEventListeners } from "./MeetingUserInterface";
-import { MeetingInfo } from "./MeetingInfo";
 
 /*
     webpack setup from 
         https://dev.to/antonmelnyk/how-to-configure-webpack-from-scratch-for-a-basic-website-46a5
 */
+
 console.log("This is my TypeScript entrypoint, dude!");
-console.log("isVideoTransformDevice: ", isVideoTransformDevice(0));
-
-
-
-// const userInterface:MeetingUserInterface = new MeetingUserInterface();
 
 document.addEventListener("DOMContentLoaded", () => {
     console.log("DOMContentLoaded, attaching event listeners");
